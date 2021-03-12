c005c2e759dea7c693fc21f1a84608bb309a1cc7
jnskender <jnskender@gmail.com>
2021-03-12 06:16:49 -0600

    add www bb-tracker to allowed domains

--- config/middleware.js
+++ config/middleware.js
@@ -9,7 +9,8 @@ module.exports = {
       "http://localhost:1337",
       "https://bb-tracker.herokuapp.com",
       "https://musing-nobel-8819d0.netlify.app",
-      "https://bb-tracker.com"
+      "https://bb-tracker.com",
+      "https://www.bb-tracker.com"
     ]
     },
   }
