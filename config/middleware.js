8946203c83415abce86dca27a926c4d5b12e3199
jnskender <jnskender@gmail.com>
2021-03-12 05:39:39 -0600

    netlify cors

--- config/middleware.js
+++ config/middleware.js
@@ -5,7 +5,10 @@ module.exports = {
     cors: {
       enabled: true,
       credentials: true,
-      origin: ["http://localhost:3000", "http://localhost:1337","https://bb-tracker.herokuapp.com"]
+      origin: ["http://localhost:3000",
+      "http://localhost:1337",
+      "https://bb-tracker.herokuapp.com",
+      "https://musing-nobel-8819d0.netlify.app"]
     },
   }
 }
